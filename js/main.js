76eff3481b3fa8494e76e7616be305aa5bb1c24a
yvaniechen <ychen363@binghamton.edu>
2021-04-30 09:44:25 -0700

    Ver.1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/* javascript */
+
+$(document).ready(function() {
+
+console.log('hello');
+
+$('#slider-inner').slick({
+     infinite: true,
+     prevArrow: $('.prev'),
+     nextArrow: $('.next'),
+   });
+
+});
